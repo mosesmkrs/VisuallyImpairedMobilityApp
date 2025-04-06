d9eb44bf56fb749c40d7c6e6a0931f000a9601a2
mosesmkrs <karisamoses392@gmail.com>
2025-04-06 18:53:23 +0300

    sqlite and emergency contacts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     // Room dependencies
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     
